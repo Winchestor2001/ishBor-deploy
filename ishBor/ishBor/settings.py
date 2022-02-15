0d3e124154915939c3ced59dd496339e6777d767
Winchestor2001 <sbexruz2001@gmail.com>
2022-02-15 17:23:47 +0500

    deploy

--- ishBor/ishBor/settings.py
+++ ishBor/ishBor/settings.py
@@ -17,6 +17,17 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-5^1twd1cbv2wzc+h!@bdn#0v-cyjthblo!zh887$eg5y*urdr@'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
 
 # Application definition
 
@@ -62,6 +73,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ishBor.wsgi.application'
 
 
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -98,9 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-
+STATIC_DIR=os.path.join(BASE_DIR,"static")
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS=[
+    STATIC_DIR,
+]
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
@@ -108,13 +130,3 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
-try:
-    from .local_settings import *
-except ImportError:
-    from .prod_settings import *
-
-
-
